5e4e13eaa60ab165f6bd440e7973a5ef3790e226
Jose <j.a.ojeda@live.com>
2019-11-01 15:20:22 -0700

    wrote History.js

---
+++ src/components/History.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Card from './Card';
+
+const History = ({ history }) => {
+
+  const cards = history.map((method, i) => {
+    return (
+      <li key={i}>
+        <Card {...method} />
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      {cards}
+    </ul>
+  );
+
+};
+
+History.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.shape({
+    route: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default History;
